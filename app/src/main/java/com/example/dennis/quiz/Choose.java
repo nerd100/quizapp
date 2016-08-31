9d6c60f7f6068d766f0f40775dfc3a84eab07826
nerd100 <den.meyer1@web.de>
2016-08-31 21:42:23 +0200

    spinner working

--- app/src/main/java/com/example/dennis/quiz/Choose.java
+++ app/src/main/java/com/example/dennis/quiz/Choose.java
@@ -1,23 +1,74 @@
 package com.example.dennis.quiz;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Debug;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Spinner;
+import android.util.Log;
+import android.widget.Toast;
 
 public class Choose extends AppCompatActivity {
+    public static final String GAME_PREFERENCES = "GamePrefs";
+    SharedPreferences shared_preferences;
+    SharedPreferences.Editor shared_preferences_editor;
+
     Button startbtn2;
+    Spinner editSpinner1, editSpinner2; //Spinner in Choose
+
+    String Category = "";
+    String Difficulty = "";
+    String test_string;
+    public void setCategory(String Category){
+        this.Category = Category;
+    }
+    public void setDifficutly(String Difficulty){
+        this.Difficulty = Difficulty;
+    }
+    public String getCategory(){
+        return Category;
+    }
+    public String getDifficulty(){
+        return Difficulty;
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_choose);
+
+
+
+
+
+
+        editSpinner1 = (Spinner) findViewById(R.id.spinner1);
+        editSpinner2 = (Spinner) findViewById(R.id.spinner2);
+
+        shared_preferences = getSharedPreferences("shared_preferences_test",
+                MODE_PRIVATE);
+
+        test_string = shared_preferences.getString("Category", "Default");
+
+        //prefEditor.putString("Diff",editSpinner2.getSelectedItem().toString());
+        //setCategory(editSpinner1.getSelectedItem().toString());
+        //setDifficutly(editSpinner2.getSelectedItem().toString());
+
+        //Log.d(TAG,"Hallo "+Category);
 
         startbtn2 = (Button) findViewById(R.id.start_btn2);
 
         startbtn2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                shared_preferences_editor = shared_preferences.edit();
+                shared_preferences_editor.putString("Category",editSpinner1.getSelectedItem().toString());
+                shared_preferences_editor.putString("Difficulty",editSpinner2.getSelectedItem().toString());
+                shared_preferences_editor.commit();
                 startActivity(new Intent(Choose.this, Start.class));
             }
         });
--- app/src/main/java/com/example/dennis/quiz/Start.java
+++ app/src/main/java/com/example/dennis/quiz/Start.java
@@ -1,50 +1,116 @@
 package com.example.dennis.quiz;
 
+import android.content.SharedPreferences;
 import android.database.Cursor;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.Random;
 
 public class Start extends AppCompatActivity {
+    SharedPreferences shared_preferences;
+    SharedPreferences.Editor shared_preferences_editor;
+
+    String test_string;
+
     DatabaseHelper myDB;
+    Choose choose;
+    TextView que;
     Button btn1,btn2,btn3,btn4;
+    String buttonName;
+    String[] parts;
+    int rightAnswer = 0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start);
 
+        shared_preferences = getSharedPreferences("shared_preferences_test",
+                MODE_PRIVATE);
+
+        test_string = shared_preferences.getString("Category", "Default");
+        Toast.makeText(getApplicationContext(), test_string, Toast.LENGTH_SHORT)
+                .show();
+        Random r = new Random();
+        rightAnswer = r.nextInt(4);
+
         myDB = new DatabaseHelper(this);
+        choose = new Choose();
+
+        buttonName=viewAll();
+        parts = buttonName.split(",");
+
+        que =  (TextView) findViewById(R.id.question1);
+        que.setText(parts[0]);
+
         btn1 = (Button) findViewById(R.id.button1);
-        viewAll();
+        btn2 = (Button) findViewById(R.id.button2);
+        btn3 = (Button) findViewById(R.id.button3);
+        btn4 = (Button) findViewById(R.id.button4);
+
+
+        mixButtons(rightAnswer,parts[1],parts[2],parts[3],parts[4]);
+
     }
 
-    public void viewAll(){
-        btn1.setOnClickListener(
-                new View.OnClickListener() {
-                    @Override
-                    public void onClick(View v) {
-                        Cursor res = myDB.getAllData("Assi","Hard");
+    public String viewAll(){
+                        Cursor res = myDB.getAllData(shared_preferences.getString("Category","Default")
+                                ,shared_preferences.getString("Difficulty","Default"));
                         if(res.getCount() == 0){
                             //show Message
-                            showMessage("Error","Nothing found!");
-                            return;
+                            //showMessage("Error","Nothing found!");
+                            return "NoData";
                         }
                         StringBuffer buffer = new StringBuffer();
                         while(res.moveToNext()){
-                            buffer.append("Question :"+ res.getString(0)+"\n");
-                            buffer.append("RA :"+res.getString(1)+"\n");
-                            buffer.append("FA1 :"+res.getString(2)+"\n");
-                            buffer.append("FA2 :"+res.getString(3)+"\n");
-                            buffer.append("FA3 :"+res.getString(4)+"\n");
+                            buffer.append(res.getString(0)+",");
+                            buffer.append(res.getString(1)+",");
+                            buffer.append(res.getString(2)+",");
+                            buffer.append(res.getString(3)+",");
+                            buffer.append(res.getString(4));
                         }
                         //Show all Data
-                        showMessage("Data",buffer.toString());
-                    }
-                }
-        );
+                        return buffer.toString();
+
     }
+
+    public void mixButtons(int i,String RA, String FA1, String FA2,String FA3){
+        switch(i){
+            case 0:
+                btn1.setText(RA);
+                btn2.setText(FA1);
+                btn3.setText(FA2);
+                btn4.setText(FA3);
+                break;
+            case 1:
+                btn1.setText(FA1);
+                btn2.setText(RA);
+                btn3.setText(FA2);
+                btn4.setText(FA3);
+                break;
+            case 2:
+                btn1.setText(FA2);
+                btn2.setText(FA1);
+                btn3.setText(RA);
+                btn4.setText(FA3);
+                break;
+            case 3:
+                btn1.setText(FA3);
+                btn2.setText(FA1);
+                btn3.setText(FA2);
+                btn4.setText(RA);
+                break;
+            default:
+                break;
+        }
+    }
+
     public void showMessage(String title,String Message){
         AlertDialog.Builder builder = new AlertDialog.Builder(this);
         builder.setCancelable(true);
